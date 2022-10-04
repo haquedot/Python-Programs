edd0131ef3cff56f39f0824a0ca9e21607c6a08f
Meraj9570 <haquemeraj95@gmail.com>
2022-10-04 15:39:50 +0530

    Calculator in python

---
+++ calculator.py
@@ -0,0 +1,20 @@
+first = int(input("Enter first number : "))
+second = int(input("Enter second number : "))
+operator = input("(+,-,*,/,%) : ")
+
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+    print(first, "+", second, "=", first + second)
+elif operator == "-":
+    print(first, "-", second, "=", first - second)
+elif operator == "*":
+    print(first, "*", second, "=", first * second)
+elif operator == "/":
+    print(first, "/", second, "=", first / second)
+elif operator == "%":
+    print(first, "%", second, "=", first % second)
+else:
+    print("Invalid Input!!!")
